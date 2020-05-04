b923fe3d71c4ea06766cdf7f38cfc7745102650f
Christopher Roush <Christopher.Roush@learninga-z.com>
2020-05-04 17:06:20 -0400

    Added sql to forms

--- www/account.php
+++ www/account.php
@@ -4,7 +4,7 @@
 <body>
 <div class="page-wrap">
     <div class="header">Create New Account </div>
-    <form class="form" action="/action_page.php">
+    <form class="form" id="newUser" action="/action_page.php">
         <label for="uname">New Username: </label>
         <input type="text" id = "uname" name="uname"><br><br>
         <label for="password">New Password: </label>
@@ -13,11 +13,36 @@
         <input type="text" id = "email" name="email"><br><br>
         <input type="button" onclick="newUser()" value = "Create New Account">
     </form>
-    <script>
-        function newUser() {
-            document.getElementById("form")
-        }
-    </script>
+    <?php
+
+    $username = $_GET["uname"];
+    $password = $_GET["password"];
+    $email = $_GET["email"];
+
+    $servername = "localhost";
+    $usernameServer = "username";
+    $passwordServer = "password";
+
+    // Create connection
+    $conn = new mysqli($servername, $usernameServer, $passwordServer);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT username FROM users WHERE username = $username";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows <= 0) {
+        $sql = "INSERT INTO users (username, password, email) VALUES ($username, $password, $email)";
+        $_SESSION['loggedin'] = True;
+        $_SESSION['login_user'] = $username;
+    } else {
+        echo "Please choose another username";
+    }
+    $conn->close();
+    ?>
     <a href = "index.php"> Back</a>
 </div>
 </body>
--- www/index.php
+++ www/index.php
@@ -4,18 +4,60 @@
 <body>
     <div class="page-wrap">
         <div class="header">Login </div>
-        <form class="form" action="/action_page.php">
+        <form class="form" id="loginForm" action="/action_page.php">
             <label for="uname">Username: </label>
             <input type="text" id = "uname" name="uname"><br><br>
             <label for="password">Password: </label>
             <input type="text" id = "password" name="password"><br><br>
             <input type="button" onclick="login()" value = "Login">
         </form>
-        <script>
-            function login() {
-                document.getElementById("form")
+        <?php
+
+        $username = $_GET["uname"];
+        $password = $_GET["password"];
+
+        $servername = "localhost";
+        $usernameServer = "username";
+        $passwordServer = "password";
+
+        // Create connection
+        $conn = new mysqli($servername, $usernameServer, $passwordServer);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT username FROM users WHERE username = $username";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows <= 0) {
+            echo "Account not found";
+        } else {
+            $sql = "SELECT password FROM users WHERE password = $password";
+            $result = $conn->query($sql);
+            while($row = mysqli_fetch_assoc($result)) {
+                if(password_verify($password, $row["password"])) {
+                    $sql = "INSERT INTO onlineUsers (username) VALUES ($username)";
+
+                    if ($conn->query($sql) === TRUE) {
+                        $_SESSION['loggedin'] = True;
+                        $_SESSION['login_user'] = $username;
+
+
+                    } else {
+                        echo "Error: " . $sql . "<br>" . $conn->error;
+                    }
+                }
+                else {
+                    echo "Wrong Password";
+                }
             }
-        </script>
+
+        }
+        $conn->close();
+        ?>
+
         <a href = "account.php"> Create New Account</a>
     </div>
     <script type="text/javascript" src="deck.js"></script>
