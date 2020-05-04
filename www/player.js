3f0ea62f68e84caeb5ca461cc25d154ee3fb528d
Q Cong <Quoc.Cong@learninga-z.com>
2020-05-04 16:21:06 -0400

    Added functions for player

--- www/player.js
+++ www/player.js
@@ -13,12 +13,70 @@
 //      check if bust
 //      calculate hand (if sum < 21 then A is 11, if not 1)
 //      changePlayer?
+import "deck.js";
 
 class Player {
     // TODO: Add member variables
-    constructor() {
-        this.name= "";
+    constructor(name) {
+        this.name = name;
+        this.money = 100;
+        this.bet = 0;
+        this.hand = [];
+        this.is_turn = false;
+        this.bust = false;
     }
 
-    // TODO: Add functions here
+    // Calculates the score of the hand
+    calcHand() {
+        var sum = 0;
+        var num_ace = 0;
+
+        for (i = 0; i < this.hand.length; i++) {
+            if (hand[i] != "A") {
+                sum += hand[i];      // TODO: Replace with correct Card implementation details
+            }
+            else {
+                num_ace += 1;
+            }
+        }
+
+        sum = this.handlesAce(sum, num_ace);
+        // sum == 0 means player is bust
+        if (sum == 0) {
+            return 22;
+        }
+        return sum;
+    }
+
+    // Returns the maximum score with aces that wouldn't bust
+    // Returns 0 if player is bust
+    handlesAce(current_score, num_ace) {
+        if (current_score > 21) {
+            return 0;
+        }
+        if (num_ace == 0) {
+            return current_score;
+        }
+        return(Math.max(this.handlesAce(current_score + 11, num_ace - 1), this.handlesAce(current_score + 1, num_ace - 1)));
+    }
+
+    checkBust() {
+        if (calcHand() > 21) {
+            this.bust = true;
+        }
+    }
+
+    placeBet(betAmount) {
+        this.bet += betAmount;
+        this.money -= betAmount;
+    }
+
+    // Draws a card from the deck and checks if the new hand is bust
+    draw(deck) {
+        var card = deck.pop();
+        this.hand.push(card);
+        
+        this.checkBust();
+    }
+    
 }
