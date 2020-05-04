ab443d17b43588cbdff8a46ab2740ba73277b3ed
Q Cong <Quoc.Cong@learninga-z.com>
2020-05-04 15:12:33 -0400

    Basic struct for player

--- www/player.js
+++ www/player.js
@@ -1,4 +1,24 @@
 // data struct for a player
-// functions to add:
-//      draw card
+// member variables:
+//      name
+//      money
+//      bet
+//      hand (cards)
+//      isTurn(?)
 //
+// functions to add:
+//      draw card (hit)
+//      stay
+//      place bet
+//      check if bust
+//      calculate hand (if sum < 21 then A is 11, if not 1)
+//      changePlayer?
+
+class Player {
+    // TODO: Add member variables
+    constructor() {
+        this.name= "";
+    }
+
+    // TODO: Add functions here
+}
