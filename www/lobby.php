649346ef48aa320bca8ec807827f51e0206fd810
Mairna Askar <Mairna.Askar@learninga-z.com>
2020-05-06 15:01:27 -0400

    Added comments to php files

--- www/lobby.php
+++ www/lobby.php
@@ -7,7 +7,7 @@
     <div> Hi <?php
 
          echo $_SESSION['login_user']?> </div>
-    <div> You have <?php
+    <div> You have <?php //setting up the server
         $servername = "localhost";
         $usernameServer = "interns2020";
         $passwordServer = "interns2020";
@@ -24,10 +24,10 @@
         $sql = "SELECT wins FROM internDatabase.users WHERE username = '$user'";
         $result = $conn->query($sql);
         while($row = mysqli_fetch_assoc($result)) {
-            echo $row['wins'];
+            echo $row['wins']; //showing the user their total amount of wins
         }?> wins. Wanna win more?</div>
     <form method="get" class="logout" id="loginForm" action="/index.php">
-        <input type="submit" name="logout" value = "Logout" >
+        <input type="submit" name="logout" value = "Logout" > <!-- logout button-->
     </form>
 
     <div class="header">Game Lobby </div>
