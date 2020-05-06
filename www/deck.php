a7aea4d79897b40e34a21b883d84941206540125
Q Cong <Quoc.Cong@learninga-z.com>
2020-05-06 11:10:45 -0400

    Added func to push deck to db. Modified Deck ctor for deckID.

--- www/deck.php
+++ www/deck.php
@@ -1,18 +1,37 @@
 <?php
-    
+    include 'helper.php';
+
     class Deck {
         private $deck;
-        private $deckNum;
-        
-        
+        private $deckID;
+
+
         public function __construct() {
             $this->deck = array();
+
+            // Peeks inside the db to get the deckID;
+            $conn = makeConnection();
+            $sql = "SELECT deckID FROM decks ORDER BY deckID DESC LIMIT 1";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    $this->deckID = $row["deckID"] + 1;
+                }
+            }
+            // Empty decks table
+            else {
+                $this->deckID = 1;
+            }
+
+            // Push new deck into db
+            $sql = "INSERT INTO decks (deckID) VALUES ('$this->deckID')";
+            $conn->query($sql);
         }
 
         public function getDeck() {
             return $this->deck;
         }
-        
+
         public function getCard() {
             return array_shift($this->deck);
         }
@@ -49,7 +68,7 @@
                 }
             }
         }
-    
+
         function shuffleDeck() {
             $deckLength = count($this->deck);
             for($i = $deckLength - 1; $i >= 0; $i--) {
@@ -59,11 +78,26 @@
                 $this->deck[$j] = $temp;
             }
         }
-    
+
         function printDeck() {
             echo '<pre>';
             print_r($this->deck);
             echo '</pre>';
+        }
+
+        function pushToDb() {
+            $conn = makeConnection();
+
+            for ($i = 0; $i < count($this->deck); $i++) {
+                $cardID = getCardID($this->deck[$i]);
+
+                // Remove old deck info from db
+                $sql = "DELETE FROM cardsDeck WHERE deckID = '$this->deckID'";
+
+                // Add cards to db
+                $sql = "INSERT INTO cardsDeck (deckID, cardID, cardOrder) VALUES ('$this->deckID', '$cardID', '$i')";
+                $conn->query($sql);
+            }
         }
     }
 ?>
