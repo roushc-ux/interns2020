2bb1d7128885b98d75bb5f12f5b5f93641718a95
Justin Hutchins <Justin.Hutchins@learninga-z.com>
2020-05-07 16:20:00 -0400

    Updated data.sql

--- www/SQL/data.sql
+++ www/SQL/data.sql
@@ -1,4 +1,4 @@
-USE internDatabase;
-INSERT INTO games (gameID, deckID, discardID, playerTurn, numPlayers) VALUES (1, 1, 1, 1, 0);
-INSERT INTO discards (discardID) VALUES (1);
-INSERT INTO decks (deckID) VALUES (1);
+USE blackjack;
+INSERT INTO game (gameID, deckID, discardID, playerTurn, numPlayers) VALUES (1, 1, 1, 1, 0);
+INSERT INTO discard (discardID) VALUES (1);
+INSERT INTO deck (deckID) VALUES (1);
