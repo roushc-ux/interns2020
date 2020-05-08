29f8a339786b78ddd75567dbf308a634d5cce8b9
Kaylina Savela <Kaylina.Savela@learninga-z.com>
2020-05-08 14:33:08 -0400

    fixed string issue in function calls

--- www/betting.php
+++ www/betting.php
@@ -3,26 +3,26 @@
 
     function takeBet($player){
         $playerID = $player->getPlayerID();
-        $row = select(money, online_user, $playerID, 'playerID');
+        $row = select("money", "online_user", $playerID, 'playerID');
         $newAmount = $row['money'] - 10;
         if ($newAmount < 0) {
-            
+            // TODO: kick player out of the game
         }
         else {
-            update(money, online_user, $newAmount, $playerID, 'playerID');
+            update("money", "online_user", $newAmount, $playerID, 'playerID');
         }
     }
 
     function addMoney($player, $money) {
         $playerID = $player->getPlayerID();
-        $row = select(money, online_user, $playerID, 'playerID');
+        $row = select("money", "online_user", $playerID, 'playerID');
         $newAmount = $row['money'] + $money;
-        update(money, online_user, $newAmount, $playerID, 'playerID');
+        update('money', 'online_user', $newAmount, $playerID, 'playerID');
         $player->addWins();
     }
 
     function getMoney($playerID) {
-        $row = select(money, online_user, $playerID, 'playerID');
+        $row = select('money', 'online_user', $playerID, 'playerID');
         return $row['money'];
     }
 ?>
