8e4384d739c79a5b577c0bcf5976914444164550
Alex London <Alex.London@learninga-z.com>
2020-05-08 15:01:57 -0400

    updated ajax to jquery(and it should actually work this time)

--- www/game2page.js
+++ www/game2page.js
@@ -1,43 +1,34 @@
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
+
+//function to check the number of players in the game to see if game should start
 function checkPlayers() {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let numPlayers = parseInt(this.response.match(/(\d+)/));
-            // console.log(typeof(numPlayers));
-            console.log(numPlayers);
-            if (numPlayers >= 3) {
-                document.getElementById("gameStart").innerHTML = "3 players have joined. Starting game.";
-                console.log("numPlayers is greater than 3");
-                clearInterval(checkStart);
-            }
-            else {
-                document.getElementById("hitBtn").disabled = true;
-                document.getElementById("stayBtn").disabled = true;
-            }
+    $.get('getNumPlayers.php', function(response) {
+        let numPlayers = parseInt(response.match(/(\d+)/));
+        if (numPlayers >= 3) {
+            document.getElementById("gameStart").innerHTML = "3 players have joined. Starting game.";
+            console.log("numPlayers is 3");
+            //clearInterval(checkStart);
         }
-    }
-    xmlhttp.open("GET", "getNumPlayers.php", true);
-    xmlhttp.send();
+        else {
+            console.log("numPlayers is less than 3");
+            document.getElementById("hitBtn").disabled = true;
+            document.getElementById("stayBtn").disabled = true;
+        }
+    })
 }
 
+//function to check the current player of the game to see if your buttons should be enabled
 function checkCurrentPlayer(playerID) {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let currentPlayer = this.response;
-            //console.log("current player");
-            //console.log(currentPlayer);
-            //console.log("player ID");
-            //console.log(playerID);
-            if (currentPlayer == playerID) {
-                document.getElementById("currentPlayer").innerHTML = "IT'S YOUR TURN";
-            }
-            else {
-                document.getElementById("hitBtn").disabled = true;
-                document.getElementById("stayBtn").disabled = true;
-            }
+    $.get('getCurrentPlayer.php', function(response) {
+        let currentPlayer = response;
+        if (currentPlayer == playerID) {
+            console.log("It's your turn");
+            document.getElementById("currentPlayer").innerHTML = "IT'S YOUR TURN";
         }
-    }
-    xmlhttp.open("GET", "getCurrentPlayer.php", true);
-    xmlhttp.send();
+        else {
+            console.log("It's not your turn");
+            document.getElementById("hitBtn").disabled = true;
+            document.getElementById("stayBtn").disabled = true;
+        }
+    })
 }
