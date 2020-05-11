226056db1c1564c279aa37fcfb9854a2ea27fd2c
Alex London <Alex.London@learninga-z.com>
2020-05-11 11:21:45 -0400

    added testing users to data.sql

--- www/SQL/data.sql
+++ www/SQL/data.sql
@@ -1,4 +1,7 @@
 USE blackjack;
 INSERT INTO game (gameID, deckID, discardID, playerTurn, numPlayers, dealerHandID) VALUES (1, NULL, NULL, 0, 0, NULL);
 INSERT INTO discard (discardID) VALUES (1);
+INSERT INTO user (username, email, password, wins) VALUES ('a', 'a@a', '$2y$10$DFUcDJs8o20QoYNDdrBwxOALai8nUF768ukRTArEvKmWD9sQNArbG', 0);
+INSERT INTO user (username, email, password, wins) VALUES ('b', 'b@b', '$2y$10$lctOnY1r0FQ34mHoWlaGXOBQv1.1HKIkEL5JuSNAjWONtNk3w6b2e', 0);
+INSERT INTO user (username, email, password, wins) VALUES ('c', 'c@c', '$2y$10$GhyP88LbOEHwg8F.uo9PbOBXpYlHeb/O0Inv5BPGKxo3pa6wup82e', 0);
 -- INSERT INTO deck (deckID) VALUES (1);
